9a1cd75ebca30801a82b8cab6016c64d9ace3f4c
playmakermz <sayaalif101@gmail.com>
2022-03-24 18:46:34 +0700

    new04

--- code/FPB.js
+++ code/FPB.js
@@ -1,21 +1,45 @@
-// 20 10 FPB
-/* 20 = 1, 2, 4, 5, 10 , 20 
- * 10 = 1, 2, 5, 10
- *  10 // 
- */
-
-function fpb(x,y){
-  let listA = []
-  let listB = []
-
-  for (let i = 1; i <= x; i++ ){
-    console.log(i)
+class FPB{
+  constructor(dataX,dataY){
+    this.x = dataX;
+    this.y = dataY;
   }
 
+  hasil(){
+    const listX = []
+    const listY = []
+    let jawaban = []
+    for (let i = 1; i <= this.x; i++){
+      if (this.x % i === 0){
+        //listX.push()
+        listX.push(i)
+      }
+    }// akhir loop x
 
-}
+    for (let i = 1; i <= this.y; i++){
+      if(this.y % i === 0){
+        listY.push(i)
+      }
+    }// akhir loop y
+    let hasil = []
+    for (let i of listX){
+      if (listY.includes(i)){
+        hasil.push(i)
+      }
+    }
 
-fpb(10,20)
+    for (let i of hasil){
+      if (i !== this.x && i !== this.y){
+        jawaban.push(i)
+      }
+    }
 
+
+    jawaban = jawaban[jawaban.length - 1];
+    console.log(jawaban)
+  }// akhir function
+}// akhir class
+
+obj = new FPB(12, 18)
+console.log(obj.hasil())
 
 

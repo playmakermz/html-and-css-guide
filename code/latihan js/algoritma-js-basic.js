a518ccb4431aafc6e9b1495c852ba8fbd95c6abc
playmakermz <sayaalif101@gmail.com>
2024-05-15 17:31:32 +0700

    obsidian Push: 05-15 17:31

--- code/latihan js/algoritma-js-basic.js
+++ code/latihan js/algoritma-js-basic.js
@@ -1,6 +1,6 @@
 // Soal
 // Input = [1,2,3,4,5,6,7]
-// output = [ 1,3,4,6,6,7 ]
+// output = [ 1,3,4,5,6,6,7 ]
 //
 // Gunakan if statement, loop, array
 
@@ -9,7 +9,7 @@ let arr = [1,2,3,4,5,6,7]
 
 for (let i = 0; i in arr; i++){
 
-    if (  i >= 1 && i < 5 ){
+    if (  arr[i] >= 2 && arr[i] < 6 ){
         console.log('masuk if', arr[i])
         arr[i] = arr[i] + 1
     }

ce5425602124cf557f76f9791e190c4758d75c2f
playmakermz <sayaalif101@gmail.com>
2024-06-07 18:28:41 +0700

    obsidian Push: 06-07 18:28

--- code/jsPro/Weather_app_API/components/constant.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-//export const API_KEY = 'dfec5a31f74e19f7cb2f1sss7c426b03a28'; // API Offline
-export const API_KEY = 'dfec5a31f74e19f7cb2f17c426b03a28';
